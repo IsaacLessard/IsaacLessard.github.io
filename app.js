9f91bfac2958981c7d96971cbb52211305a7fc54
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2015-11-11 07:29:46 -0700

    new styling

--- app.js
+++ app.js
@@ -12,10 +12,22 @@ $( document ).ready(function() {
         }
       }
     });
-    // $('window').load(function(){
-		// $('.circleNav').fadeIn("slow", function(){
-		// 	$(this).addClass("ungrey");
+    // $('.main').load(function(){
+		//     $('.circleNav').fadeIn("slow", function(){
+		// 	       $('.circleNav').addClass("ungrey");
 	  //    });
     //  });
   });
 });
+
+var $para = "<p>does it work</p>";
+
+$('.circImg').hover(function(){
+  $(this).next().text('Get amongst it');
+});
+
+var circleImage = document.getElementsByClassName('circImg');
+
+function resetIt(event){
+
+}
